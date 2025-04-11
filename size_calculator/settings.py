9529d7e86848454c71100643a8b5a16fd8f5f962
0layimika <110624406+0layimika@users.noreply.github.com>
2025-04-11 20:35:41 +0100

    updated allowed hosts

--- size_calculator/settings.py
+++ size_calculator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-po#^s#a+*@e$%6_rvnv_hthv(gcgdfib7ensma73s^bzy$*355
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
